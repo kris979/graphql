b25a329badad80a1f023c2d8f65f1614e61e9a72
kris <kris979@gmail.com>
2020-03-11 17:25:24 +0000

    removing altair

--- src/main/java/com/agisoft/graphql/controllers/Controlller.java
+++ src/main/java/com/agisoft/graphql/controllers/Controlller.java
@@ -1,13 +1,16 @@
 package com.agisoft.graphql.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Controlller {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("Returning hello");
         return "<h1>Hello</h1>";
     }
 
